995984abba5df72d39850d549be8ff99bde1a01e
Saar Seri <saarseri93@gmail.com>
2021-06-28 15:42:25 +0300

    find Text key byWidget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,36 +13,61 @@ import '../lib/main.dart';
 
 void main() {
   print("test Widget2");
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    print("testWidgets3");
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   print("testWidgets3");
 
-    await tester.pumpWidget(MyApp());
+  //   await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
+  //   // expect(find.text('1'), fin);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
 
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  //   await tester.tap(find.byIcon(Icons.add));
+  //       //!not working// await tester.tap(find.byWidget(Icon(Icons.add)));
 
-    // Verify that our counter has incremented.
-    expect(find.text('1'), findsNothing);
-    expect(find.text('2'), findsOneWidget);
+  //   await tester.pump();
 
-    await tester.tap(find.byIcon(Icons.remove));
-    await tester.pump();
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('1'), findsNothing);
+  //   expect(find.text('2'), findsOneWidget);
+  //   //!not working// expect(find.byWidget(Text("2")) ,  findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('2'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  //   await tester.tap(find.byIcon(Icons.remove));
+  //   await tester.pump();
+
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('2'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+
+// testWidgets("using key", (WidgetTester tester) async{
+//   const testKey= Key('k');
+//     await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
+
+//   // Find the MaterialApp widget using the testKey.
+//   expect(find.byKey(testKey), findsOneWidget);
+
+// },
+// );
+  testWidgets('finds a specific instance', (WidgetTester tester) async {
+    const childWidget = Padding(padding: EdgeInsets.zero);
+
+    // Provide the childWidget to the Container.
+    await tester.pumpWidget(Container(child: childWidget));
+
+    // Search for the childWidget in the tree and verify it exists.
+    expect(find.byWidget(childWidget), findsOneWidget); //V
+    // expect(Padding(padding: EdgeInsets.zero), findsNothing);//fail
+    // expect(Padding(padding: EdgeInsets.zero), findsOneWidget);//fail
   });
 }
