c3e85d4e29507c16db5c7c8c63a445508dede396
Saar Seri <saarseri93@gmail.com>
2021-06-28 15:59:46 +0300

    no finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,58 @@
-import 'dart:async';
-import 'dart:convert';
-import './myHome.dart';
+ import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
+ main(TodoList args) {
+   runApp(args())
+ }
+class TodoList extends StatefulWidget {
+  const TodoList({Key  key}) : super(key: key); 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
+  _TodoListState createState() => _TodoListState();
+} 
+class _TodoListState extends State<TodoList> {
+  static const _appTitle = 'Todo List';
+  final todos = <String>[];
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: _appTitle,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Fetch Data Example'),
+          title: const Text(_appTitle),
         ),
-        body: CounterPage(),
+        body: Column(
+          children: [
+            TextField(
+              controller: controller,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final todo = todos[index];
+
+                  return Dismissible(
+                    key: Key('$todo$index'),
+                    onDismissed: (direction) => todos.removeAt(index),
+                    child: ListTile(title: Text(todo)),
+                    background: Container(color: Colors.red),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              todos.add(controller.text);
+              controller.clear();
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
