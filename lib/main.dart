94b014fdc8e24463923791a6f29266d9d681912e
Saar Seri <saarseri93@gmail.com>
2021-06-28 14:31:05 +0300

    mock http

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,16 @@ Future<Album> fetchAlbum(http.Client client) async {
 }
 
 class Album {
-  // final int userId;
-  // final int id;
+  final int userId;
+  final int id;
   final String title;
 
-  // Album({@required this.userId, @required this.id, @required this.title});
-Album({ @required this.title});
+  Album({@required this.userId, @required this.id, @required this.title});
+// Album({ @required this.title});
   factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
-      // userId: json['userId'],
-      // id: json['id'],
+      userId: json['userId'],
+      id: json['id'],
       title: json['title'],
     );
   }
